1ac5abeeb0a121a1857911d123560314dbc385fe
koedi <45913058+koedi@users.noreply.github.com>
2019-01-03 22:30:46 +0200

    Update MontyHall.scala

--- MontyHall.scala
+++ MontyHall.scala
@@ -1,76 +1,84 @@
 object MontyHall extends App {
 
-  class Room() {
+  class Room(val id: Int) {
     private var prize = false;
     private var selected = false;
-      
+
     def setPrize: Unit = { this.prize = true }
-    def isWin: Boolean = { this.prize }
+    def hasPrize: Boolean = { this.prize }
 
     def setSelected: Unit = { this.selected = true }
     def isSelected: Boolean = { this.selected }
 
-    def reset: Unit = {
-      this.selected = false
-      this.prize = false
-    }
-
+    def reset: Unit = { this.selected = false; this.prize = false }
+    
+    override def toString = "ID: "+ id +" S: " + this.selected + " P: " + this.prize + " "
+    
   } // class ROOM
 
-  
   class Game(val rooms: Vector[MontyHall.Room]) {
-
+    
     def resetGame(): Unit = {
       this.rooms.foreach(_.reset)
     }
 
-    def dropAllButOne(): Vector[MontyHall.Room] = {
-      this.rooms.filter(_.isSelected)
+    def getSelectedRoom(): MontyHall.Room = {
+      this.rooms.filter(_.isSelected).head
     }
 
-    def changeChoice(): Vector[MontyHall.Room] = {
-      this.rooms.find(x => !x.isSelected && !x.isWin).get.setSelected
-      this.rooms.filter(!_.isSelected)
+    def changesChoice(): Int = {
+      this.getSelectedRoom().hasPrize match {
+        case true  => 0
+        case false => 1
+      }
     }
-    
+
+    def doesNotChangeChoice(): Int = {
+      this.getSelectedRoom().hasPrize match {
+        case true  => 1
+        case false => 0
+      }
+    }
+
     def setChoices(): Unit = {
-      this.rooms(rnd.nextInt(3)).setSelected
-      this.rooms(rnd.nextInt(3)).setPrize
+      this.rooms(rnd.nextInt(this.rooms.size)).setSelected
+      this.rooms(rnd.nextInt(this.rooms.size)).setPrize
+      
     }
-   
+
   } //class GAME
   
 
   /**
-   * MAIN PART
+   * MONTY HALL SIMULATOR
    */
 
   var runs = 1000
   var i = 0
 
-  val rnd = new scala.util.Random
-  val simulatedGame = Vector(new Room(), new Room(), new Room())
 
-  val simGame = new Game(Vector(new Room(), new Room(), new Room()))
+  val rnd = new scala.util.Random
+  
+  val simGame = new Game(Vector(new Room(1), new Room(2), new Room(3)))
   
   var noChangeWins = 0
   var changeWins = 0
  
   
-  while ( i <= runs) {
+  while ( i < runs) {
+    simGame.resetGame()
     simGame.setChoices()
 
-    if (simGame.dropAllButOne().head.isWin)
-      noChangeWins += 1
-            
-    if (simGame.changeChoice().head.isWin)
-      changeWins += 1  
+    // Does not change room
+    noChangeWins += simGame.doesNotChangeChoice()
       
-    simGame.resetGame()
+    // Changes room
+    changeWins += simGame.changesChoice()
+      
     i += 1
   }
   
   println("Runs: " + runs)
   println("No change: " + 1.0*noChangeWins/runs + "\nChange:\t   " + 1.0*changeWins/runs)
-
-}
+  
+ }
