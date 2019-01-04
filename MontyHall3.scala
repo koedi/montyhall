287dab94adafcf11cb1c2d406843e56d63d76d0f
koedi <45913058+koedi@users.noreply.github.com>
2019-01-04 07:06:12 +0200

    Update MontyHall3.scala

--- MontyHall3.scala
+++ MontyHall3.scala
@@ -12,10 +12,10 @@
   var changeWins = 0
   var noChangeWins = 0
   
-  for ( i<- 0 to runs) {
+  for (i <- 0 to runs) {
     prize = rnd.nextInt(3)
     selected = rnd.nextInt(3)
-    tempChange +=    {if (prize == selected) 0 else 1}
+    tempChange   +=  {if (prize == selected) 0 else 1}
     tempNoChange +=  {if (prize == selected) 1 else 0}
   }
   
