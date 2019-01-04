1bc0aa34c0f36e49fd77aaadd22b495bbb0999fc
koedi <45913058+koedi@users.noreply.github.com>
2019-01-04 06:48:45 +0200

    Add files via upload

---
+++ MontyHall2.scala
@@ -0,0 +1,88 @@
+object MontyHall extends App {
+
+  class Room(val id: Int) {
+    private var prize = false;
+    private var selected = false;
+
+    def setPrize: Unit = { this.prize = true }
+    def hasPrize: Boolean = { this.prize }
+
+    def setSelected: Unit = { this.selected = true }
+    def isSelected: Boolean = { this.selected }
+
+    def reset: Unit = { this.selected = false; this.prize = false }
+    
+    override def toString = "ID: "+ id +" S: " + this.selected + " P: " + this.prize + " "
+    
+  } // class ROOM
+
+  class Game(val rooms: Vector[MontyHall.Room]) {
+    
+    def resetGame(): Unit = {
+      this.rooms.foreach(_.reset)
+    }
+
+    def getSelectedRoom(): MontyHall.Room = {
+      this.rooms.filter(_.isSelected).head
+    }
+
+    def changesChoice(): Int = {
+      /*
+      this.getSelectedRoom().hasPrize match {
+        case true  => 0
+        case false => 1
+      }
+			*/      
+      
+       this.rooms.find(x => !x.isSelected && !x.hasPrize).get.setSelected
+       bool2int(this.rooms.filter(!_.isSelected).head.hasPrize)
+    }
+
+    def doesNotChangeChoice(): Int = {
+      /*
+      this.getSelectedRoom().hasPrize match {
+        case true  => 1
+        case false => 0
+      }
+      */
+      
+      bool2int(this.getSelectedRoom().hasPrize)
+    }
+
+    def setChoices(): Unit = {
+      this.rooms(rnd.nextInt(this.rooms.size)).setSelected
+      this.rooms(rnd.nextInt(this.rooms.size)).setPrize      
+    }
+
+    implicit def bool2int(b:Boolean) = if (b) 1 else 0
+    
+  } //class GAME
+  
+
+  /**
+   * MONTY HALL SIMULATOR
+   */
+
+  var runs = 10000
+  
+  val rnd = new scala.util.Random
+  val simGame = new Game(Vector(new Room(1), new Room(2), new Room(3)))
+
+  var noChangeWins = 0
+  var changeWins = 0
+
+  for(i <- 0 to runs) {
+    simGame.resetGame()
+    simGame.setChoices()
+
+    // Does not change room
+    noChangeWins += simGame.doesNotChangeChoice()
+      
+    // Changes room
+    changeWins += simGame.changesChoice()
+  }
+  
+  println("Runs: " + runs)
+  println("No change: " + 1.0*noChangeWins/runs + "\nChange:\t   " + 1.0*changeWins/runs)
+  
+ }
